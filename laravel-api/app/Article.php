22a6f0ec0a664bf8f64be2bc25db336c44a926b5
Damian Linek <damianudl@gmail.com>
2018-06-24 13:30:57 +0200

    Post mcr

--- laravel-api/app/Article.php
+++ laravel-api/app/Article.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'title', 'body',
+    ];
 }
--- laravel-api/database/migrations/2018_06_24_112810_create_posts_table.php
+++ laravel-api/database/migrations/2018_06_24_112810_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->text('body');
+            $table->text('article_id');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('posts');
     }
 }
